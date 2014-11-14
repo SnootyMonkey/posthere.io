fd835e847cf1fc7c020a095ac4c703c23bbb1b8b
Sean Johnson <sean@snootymonkey.com>
2014-11-14 08:26:51 -0500

    Finish up the Redis storage and reading of requests, and tests of the same.

--- test/posthere/unit/storage.clj
+++ test/posthere/unit/storage.clj
@@ -5,35 +5,56 @@
             [taoensso.carmine :as car]
             [clj-time.core :as t]
             [posthere.util.uuid :refer (uuid)]
-            [posthere.storage :as storage :refer (day request-separator)]))
+            [posthere.storage :as storage :refer (day request-separator request-storage-count)]))
 
 (facts "about storing requests"
 
-  (future-fact "requests can be stored")
-    ;; Store
+  (future-fact "requests can be stored in the request list"
+    (let [url-uuid (uuid)]
+      (storage/save-request url-uuid {})
+      (storage/wcar*
+        (car/exists (storage/url-key-for url-uuid))) => true
+        (car/llen (storage/url-key-for url-uuid)) => 1))
   
-  (future-fact "stored requests can be retrieved")
-    ;; Store, Read, check expires
-  
-  (fact "request lists expire"
+  (fact "stored request lists expire"
     (let [url-uuid (uuid)]
       (storage/save-request url-uuid {})
       (storage/wcar*
         (car/ttl (storage/url-key-for url-uuid))) => (roughly day 10)))
 
-  (future-fact "request list entries contain an expiration and a request UUID")
+  (fact "request list entries contain an expiration and a request UUID"
+    (let [url-uuid (uuid)]
+      (storage/save-request uuid {})
+      (let [request-entry (storage/wcar* (car/lpop (storage/url-key-for uuid)))]
+          (count (storage/parse-request-entry request-entry)) => 2)))
 
   (fact "stored requests expire"
     (let [url-uuid (uuid)]
       (storage/save-request uuid {})
       (let [request-entry (storage/wcar* (car/lpop (storage/url-key-for uuid)))
-            request-uuid (last (s/split request-entry (re-pattern (str "\\" request-separator))))]
-        (println request-entry)
-        (println request-uuid)
+            request-uuid (storage/uuid-from-entry request-entry)]
         (storage/wcar* (car/ttl (storage/request-key-for request-uuid))) => (roughly day 10))))
 
+  (facts "stored requests can be retrieved"
+    ;; store 1 request
+    (let [request {:cool :beans}
+          url-uuid (uuid)]
+      (storage/save-request url-uuid request)
+      (storage/requests-for url-uuid) => [request])
+    ;; store many requests
+    (let [requests [{:cool :beans}{:rotten :bananas}{:evolved :ferrets}]
+          url-uuid (uuid)]
+      (doseq [request requests]
+        (storage/save-request url-uuid request))
+      (storage/requests-for url-uuid) => (reverse requests)))
 
-  (future-fact "only the last 100 requests are stored")
-    ;; Store 101
-    ;; Read, get 100
-)
+  (fact "only the last request-storage-count requests are stored"
+    (let [request {:cool :beans}
+          url-uuid (uuid)]
+      ;; store too many requests!
+      (dotimes [n (+ request-storage-count 2)]
+        (storage/save-request url-uuid request))
+      ; but there's still only the max
+      (let [requests (storage/requests-for url-uuid)]
+        (count requests) => request-storage-count
+        requests => (repeat request-storage-count request)))))
