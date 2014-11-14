724cbdae1d2837c178c0a9065b88f2c338c150d4
Eric Musgrove <emusgrove@path.com>
2014-11-14 10:31:05 -0500

    Conflict resolution

--- src/posthere/app.clj
+++ src/posthere/app.clj
@@ -6,18 +6,25 @@
               [compojure.core :refer :all]
               [compojure.route :as route]
               [org.httpkit.server :refer (run-server)]
-              [environ.core :refer (env)]))
+              [environ.core :refer (env)]
+              [posthere.storage :refer [save-request, requests-for]]))
 
 (defonce hot-reload (or (env :hot-reload) true))
 
 (defn template-for [name] (str "../resources/html/" name))
 
 ; Results page work
-(deftemplate results-page (template-for "results.html") [] 
-  [:head :title] (html-content (str "POSThere.io - Results")))
+(deftemplate results-page (template-for "results.html") [results] 
+  [:head :title] (html-content (str "POSThere.io - Results"))
+  [:#results] (if (< (count results) 1) (html-content (str "Foo")))
+  [:#empty-results] [:#results] (if (< (count results) 1) nil))
 
-(defn- results-view []
-  (apply str (results-page)))
+; storing is easy:  (save-request url-uuid request-hash)
+; reading them back is easy:  (requests-for url-uuid)
+
+(defn- results-view [uuid]
+  (let [results (requests-for uuid)]
+    (apply str (results-page results))))
 ; Results page work
 
 ; POST results
@@ -29,13 +36,13 @@
 
 (defroutes approutes
   ; GET requests
-  (GET "/:uuid" [uuid] (results-view))
-  (GET "/:status/:uuid" [status, uuid] (results-view))
-  (GET "/" [] (str "Home from http-kit"))
+  (GET "/:uuid" [uuid] (results-view uuid))
+  (GET "/" [] (str "Home From Nginx"))
 
   ; POST requests
-  (POST "/:uuid" [uuid] (post-results uuid 200))
-  (POST "/:status/:uuid" [status, uuid] (post-results uuid status))
+  (POST "/:uuid" [uuid :as request]
+    (let [body (:body request)]
+      (post-results (slurp body) uuid)))
 
   ; Standard requests
   (route/resources "/assets/")
