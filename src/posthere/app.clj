3f3ccb526868edfbb83548fbfe27cadb8364e553
Eric Musgrove <emusgrove@path.com>
2014-11-15 01:39:28 -0500

    Working on getting the views setup

--- src/posthere/app.clj
+++ src/posthere/app.clj
@@ -1,36 +1,45 @@
 (ns posthere.app
   "PostHere.io web application."
   (:gen-class)
-    (:use     [net.cgrand.enlive-html]
-              [json-html.core])
+    (:use     [net.cgrand.enlive-html])
     (:require [ring.middleware.reload :as reload]
               [compojure.core :refer :all]
               [compojure.route :as route]
               [org.httpkit.server :refer (run-server)]
               [environ.core :refer (env)]
-              [posthere.storage :refer [save-request, requests-for]]))
+              [posthere.storage :refer [save-request, requests-for]]
+              [clojure.data.json :as json]))
 
 (defonce hot-reload (or (env :hot-reload) false))
 
 (defn template-for [name] (str "../resources/html/" name))
 
 ; Results page work
+(defsnippet result-rows (template-for "result-table.html")
+  [:.result-group]
+  [replacements]
+  [:.result-group any-node] (replace-vars replacements))
+
 (deftemplate results-page (template-for "results.html") [results, uuid] 
   [:head :title] (html-content (str "POSThere.io - Results"))
 
   ; any idea how we would clean this up?
   [:#results] (if (not-empty results) 
-                (do->
-                  (remove-attr :style)
-                  (html-content results)))
+                  (remove-attr :style))
+  
   [:#empty-results] (if (not-empty results)
                       nil
-                      (append ""))
+                      (append (str "")))
+
+  [:#data] (if (not-empty results)
+              (html-content (str "<script type='text/javascript'>var resultData = " (json/write-str results) "</script>"))
+              (html-content (str "<script type='text/javascript'>var resultData = []; </script>")))
+
   [:.uuid-value] (html-content (str uuid)))
 
 (defn- results-view [uuid]
   (let [results (requests-for uuid)]
-    (doseq [item results] (prn item))
+    ; (doseq [item results] (prn (:headers item)))
     (apply str (results-page results uuid))))
 
 (defn- update-request-body-too-big [uuid, request-hash]
