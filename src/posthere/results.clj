5707943080dcfec29a065a54d5cba6e6528a0f65
Sean Johnson <sean@snootymonkey.com>
2014-11-28 11:04:43 -0500

    Replace placeholder cURL examples with real ones.

--- src/posthere/results.clj
+++ src/posthere/results.clj
@@ -8,19 +8,24 @@
 
 (deftemplate results-page (template-for "results.html") [results, uuid] 
 
+  ;; unhide the results div if we DO have some results
   [:#results] (if (not-empty results) 
                   (enl/remove-attr :style))
   
-  [:#empty-results] (if (not-empty results)
-                      nil ; clear out the empty results copy
-                      (enl/append (str ""))) ;; no-op
+  ;; remove the empty-results div if we DO have some results
+  [:#empty-results] (if (empty? results)
+                      (enl/append (str ""))) ; clear out the empty results HTML copy
 
-  [:#data] (if (not-empty results)
-    (enl/html-content (str "<script type='text/javascript'>var resultData = " (generate-string results) "</script>"))
-    (enl/html-content (str "<script type='text/javascript'>var resultData = []; </script>")))
+  ;; add a JavaScript array with the POST results for this UUID from redis (or an empty array if there are none)
+  [:#data] (if (empty? results)
+            (enl/html-content (str "<script type='text/javascript'>var resultData = []; </script>"))
+            (enl/html-content (str "<script type='text/javascript'>var resultData = " (generate-string results) "</script>")))
 
+  ;; replace the placeholder UUID in the HTML template with our actual UUID
   [:.uuid-value] (enl/html-content (str uuid)))
 
-(defn results-view [uuid]
+(defn results-view
+  "Create our HTML page for results using the results HTML template and enlive."
+  [uuid]
   (let [results (requests-for uuid)]
     (apply str (results-page results uuid))))
