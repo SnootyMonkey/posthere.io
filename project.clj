56a3e3171a79c6bf5cf53c25cc3ca5c065fb5e02
Sean Johnson <sean@snootymonkey.com>
2014-12-11 06:37:43 -0500

    Update dependencies.

--- project.clj
+++ project.clj
@@ -23,10 +23,10 @@
     [http-kit "2.1.19"] ; Web Server http://http-kit.org/
     [compojure "1.3.1"] ; Web routing https://github.com/weavejester/compojure
     [enlive "1.1.5"] ; HTML Templating system for Clojure https://github.com/cgrand/enlive
-    [com.taoensso/carmine "2.8.0"] ; Redis client for Clojure https://github.com/ptaoussanis/carmine
+    [com.taoensso/carmine "2.9.0"] ; Redis client for Clojure https://github.com/ptaoussanis/carmine
     [clj-time "0.9.0-beta1"] ; Clojure date/time library https://github.com/clj-time/clj-time
     [environ "1.0.0"] ; Get environment settings from different sources https://github.com/weavejester/environ
-    [cheshire "5.3.1"] ; JSON de/encoding https://github.com/dakrone/cheshire
+    [cheshire "5.4.0"] ; JSON de/encoding https://github.com/dakrone/cheshire
     [org.clojure/data.xml "0.0.8"] ; XML parser/encoder https://github.com/clojure/data.xml
     ;; Client-side
     [org.clojure/clojurescript "0.0-2411"] ; ClojureScript compiler https://github.com/clojure/clojurescript
