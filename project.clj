27502b92dbf0df7c0d706ce6dbc5dc29d7de13de
Sean Johnson <sean@snootymonkey.com>
2015-05-23 19:58:33 -0400

    Update dependencies.

--- project.clj
+++ project.clj
@@ -14,11 +14,11 @@
 
   :dependencies [
     ;; Server-side
-    [org.clojure/clojure "1.7.0-beta3"] ; Lisp on the JVM http://clojure.org/documentation
+    [org.clojure/clojure "1.7.0-RC1"] ; Lisp on the JVM http://clojure.org/documentation
     [org.clojure/core.match "0.3.0-alpha4"] ; Erlang-esque pattern matching https://github.com/clojure/core.match
     [defun "0.2.0-RC"] ; Erlang-esque pattern matching for Clojure functions https://github.com/killme2008/defun
-    [ring/ring-devel "1.4.0-beta2"] ; Web application library https://github.com/ring-clojure/ring
-    [ring/ring-core "1.4.0-beta2"] ; Web application library https://github.com/ring-clojure/ring
+    [ring/ring-devel "1.4.0-RC1"] ; Web application library https://github.com/ring-clojure/ring
+    [ring/ring-core "1.4.0-RC1"] ; Web application library https://github.com/ring-clojure/ring
     [http-kit "2.1.19"] ; Development Web server http://http-kit.org/
     [compojure "1.3.4"] ; Web routing https://github.com/weavejester/compojure
     [jumblerg/ring.middleware.cors "1.0.1"] ; CORS library https://github.com/jumblerg/ring.middleware.cors
@@ -30,7 +30,7 @@
     [cheshire "5.4.0"] ; JSON de/encoding https://github.com/dakrone/cheshire
     [org.clojure/data.xml "0.0.8"] ; XML parser/encoder https://github.com/clojure/data.xml
     ;; Client-side
-    [org.clojure/clojurescript "0.0-3269"] ; ClojureScript compiler https://github.com/clojure/clojurescript
+    [org.clojure/clojurescript "0.0-3297"] ; ClojureScript compiler https://github.com/clojure/clojurescript
     [jayq "2.5.4"] ; ClojureScript wrapper for jQuery https://github.com/ibdknox/jayq
     [hiccups "0.3.0"] ; ClojureScript implementation of Hiccup https://github.com/teropa/hiccups
   ]
