409db560b5257b22997ad0c9e0eab09854c2a429
Sean Johnson <sean@snootymonkey.com>
2015-07-07 06:49:28 -0400

    Update dependency.

--- project.clj
+++ project.clj
@@ -25,7 +25,7 @@
     [raven-clj "1.3.1"] ; Clojure interface to Sentry error reporting https://github.com/sethtrain/raven-clj
     [enlive "1.1.5"] ; HTML Templating system for Clojure https://github.com/cgrand/enlive
     [com.taoensso/carmine "2.11.1"] ; Redis client for Clojure https://github.com/ptaoussanis/carmine
-    [clj-time "0.9.0"] ; Clojure date/time library https://github.com/clj-time/clj-time
+    [clj-time "0.10.0"] ; Clojure date/time library https://github.com/clj-time/clj-time
     [environ "1.0.0"] ; Get environment settings from different sources https://github.com/weavejester/environ
     [cheshire "5.5.0"] ; JSON de/encoding https://github.com/dakrone/cheshire
     [org.clojure/data.xml "0.0.8"] ; XML parser/encoder https://github.com/clojure/data.xml
